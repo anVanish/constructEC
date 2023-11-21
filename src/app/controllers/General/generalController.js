d2073a0f54f11992b8ea388672f09c4ab8768fde
Vanish <sosvanish@gmail.com>
2023-11-21 23:21:08 +0700

    upate general views

--- src/app/controllers/General/generalController.js
+++ src/app/controllers/General/generalController.js
@@ -3,42 +3,40 @@ class GeneralController{
 
     //GET /gioi-thieu
     about(req, res, next){
-        // const {desc, address, phone, name} = shop
-        res.render('general/about', {
+        res.render('about', {
             shop: req.shop,
             cate: req.cate,
             pageAbout: true
         })
-        req.success = true
+        
     }
 
     //GET /lien-he
     contact(req, res, next){
-        res.render('general/contact', {
+        res.render('contact', {
             shop: req.shop,
             cate: req.cate,
             pageContact: true,
         })
-        req.success = true
+        
     }
 
     //GET /cong-trinh
     construct(req, res, next){
-        res.render('general/construct', {
+        res.render('construct', {
             shop: req.shop,
             cate: req.cate,
             pageConstruct: true,
         })
-        req.success = true
+        
     }
 
     //GET /dich-vu
     service(req, res, next){
-        res.render('general/service', {
+        res.render('service', {
             shop: req.shop,
             pageService: true,
         })
-        req.success = true
     }
 }
 
